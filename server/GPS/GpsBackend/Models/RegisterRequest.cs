e4f41f2077dff19bfef253344d258447daf97476
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-10-24 22:21:24 +0100

    Corrected typo in LastName

--- server/GPS/GpsBackend/Models/RegisterRequest.cs
+++ server/GPS/GpsBackend/Models/RegisterRequest.cs
@@ -3,7 +3,7 @@
     public class RegisterRequest
     {
         public string FirstName { get; set; }
-        public string LasttName { get; set; }
+        public string LastName { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
         public string PostCode { get; set; }
