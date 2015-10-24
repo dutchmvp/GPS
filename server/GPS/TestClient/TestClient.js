d5680ff17311b17b27182cca9521114244aa5a6d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-10-24 18:23:37 +0100

    Now sending JSON from the test client

--- server/GPS/TestClient/TestClient.js
+++ server/GPS/TestClient/TestClient.js
@@ -4,29 +4,43 @@
 
     var baseUrl = "http://localhost:57911/";
 
-    $(document).ready(function() {
-        $("#btnSendLocationData").click(function() {
-            $.post(
-                baseUrl + "api/client/location",
-                {
-                    "id": "Granny Smith",
-                    "timestamp": 1234567890,
-                    "locations": [
-                        {
-                            "uuid": "D9B9EC1F-3925-43D0-80A9-1E39D4CEA95C",
-                            "majorNumber": 1,
-                            "minorNumber": 3,
-                            "strength": 67
-                        },
-                        {
-                            "uuid": "mac address",
-                            "majorNumber": 1,
-                            "minorNumber": 2,
-                            "strength": 67
-                        }
-                    ]
-                }
-            );
+    $(document).ready(function () {
+
+        var id = "Granny Smith";
+
+        var getTimestamp = function () {
+            return Math.floor((new Date()).getTime() / 1000);
+        };
+
+        var postJson = function (path, data) {
+            data.id = id;
+            data.timestamp = getTimestamp();
+            return $.ajax({
+                url: baseUrl + path,
+                method: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            });
+        };
+
+        $("#btnSendLocationData").click(function () {
+            var data = {
+                "locations": [
+                    {
+                        "uuid": "D9B9EC1F-3925-43D0-80A9-1E39D4CEA95C",
+                        "majorNumber": 1,
+                        "minorNumber": 3,
+                        "strength": 67
+                    },
+                    {
+                        "uuid": "mac address",
+                        "majorNumber": 1,
+                        "minorNumber": 2,
+                        "strength": 67
+                    }
+                ]
+            };
+            postJson("api/client/location", data);
         });
     });
 }());
